643c3c31ea542d5c947a2890f50b0aed5e99d4ac
Carlos Júnior <carlosjunior@Carloss-iMac.local>
2018-12-09 17:36:37 -0200

    Commit inicial

---
+++ quizNight/quizNight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quizNight
+//
+//  Created by Carlos Júnior on 09/12/18.
+//  Copyright © 2018 CarlosJR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
